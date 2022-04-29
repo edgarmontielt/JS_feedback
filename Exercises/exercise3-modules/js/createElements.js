4d9735ea2500185b3def523f66f3c1760fed4a68
EdgarMTdev <edgarmontiel961@gmail.com>
2022-04-29 09:54:07 -0500

    refactor dnd functions

--- Exercises/exercise3-modules/js/createElements.js
+++ Exercises/exercise3-modules/js/createElements.js
@@ -1,14 +1,11 @@
-import { dragEnter, dragLeave, dragOver, onDrop } from "./dragAndDrop.js"
+import { addEventsToDropzone, addEventsToElement } from "./dragAndDrop.js"
 
 const createDropzones = (dropzones, container) => {
      dropzones.forEach(() => {
           const div = document.createElement('div')
           div.className = 'dropzone'
           container.appendChild(div)
-          dragEnter(div)
-          dragLeave(div)
-          dragOver(div)
-          onDrop(div)
+          addEventsToDropzone(div)
      })
 }
 
@@ -20,17 +17,7 @@ const createElements = (elements, container) => {
           p.className = 'element'
           p.id = `item${element}`
           firstDiv.appendChild(p)
-          p.draggable = true
-
-          p.ondrag = () => {
-               p.style.opacity = '0'
-          }
-          p.ondragstart = (event) => {
-               event.dataTransfer.setData('item', event.target.id)
-          }
-          p.ondragend = () => {
-               p.style.opacity = '1'
-          }
+          addEventsToElement(p)
      })
 }
 
--- Exercises/exercise3-modules/js/dragAndDrop.js
+++ Exercises/exercise3-modules/js/dragAndDrop.js
@@ -1,28 +1,34 @@
-const dragEnter = (element) => {
+const addEventsToDropzone = (element) => {
      element.ondragenter = () => {
           element.style.border = '4px solid #16aa4a'
      }
-}
-
-const dragLeave = (element) => {
      element.ondragleave = () => {
+          element.style.border = ''
+     }
+     element.ondragover = (event) => {
+          event.preventDefault()
+     }
+     element.ondrop = (event) => {
+          event.preventDefault()
+          const item = event.dataTransfer.getData('item')
+          element.appendChild(document.getElementById(item))
           element.style.border = ''
      }
 }
 
-const dragOver = (element) => {
-     element.ondragover = (event) => {
-          event.preventDefault()
+
+const addEventsToElement = (element) => {
+     element.draggable = true
+
+     element.ondragstart = (event) => {
+          event.dataTransfer.setData('item', event.target.id)
+     }
+     element.ondrag = () => {
+          element.style.opacity = '0'
+     }
+     element.ondragend = () => {
+          element.style.opacity = '1'
      }
 }
 
-const onDrop = (dropzone) => {
-     dropzone.ondrop = (event) => {
-          event.preventDefault()
-          const item = event.dataTransfer.getData('item')
-          dropzone.appendChild(document.getElementById(item))
-          dropzone.style.border = ''
-     }
-}
-
-export {dragEnter, dragLeave, dragOver, onDrop}
+export { addEventsToDropzone, addEventsToElement }
