09b86e3253cf2582fa55fc87d11c351c54f95800
EdgarMTdev <edgarmontiel961@gmail.com>
2022-05-13 19:05:22 -0500

    Add styles

--- Exercises/exercise5-forms/index.js
+++ Exercises/exercise5-forms/index.js
@@ -1,23 +1,65 @@
+'use strict'
+
 const form = document.getElementById('form')
 const urlValidation = document.getElementById('url_validation')
+const emailValidation = document.getElementById('email_validation')
+
+const urlRegEx = /https?:\/\/[\w]*\.[\w]*[\.|\/]?[\w]*[\.|\/]?[\w|\/-]*/
+const emailRegEx = /^[\w.]+@[\w]+\.{1}[\w]+(\.{1}[\w]+)*$/
+
+const validation = () => {
+    const url = form.email.value
+    const email = form.email.value
+
+    if (urlRegEx.test(url)) {
+        urlValidation.innerText = 'Es valido👍'
+        urlValidation.style.color = 'green'
+    } else {
+        urlValidation.innerText = 'No es válido😡'
+    }
+
+    if (emailRegEx.test(email)) {
+        emailValidation.innerText = 'Es válido'
+        form.email.className = 'success'
+        emailValidation.style.color = 'green'
+
+        setTimeout(() => {
+            emailValidation.innerText = ''
+        }, 2000)
+    } else {
+        emailValidation.innerText = 'No es valido'
+        form.email.className = 'error'
+    }
+}
+
+
+form.email.oninput = validation
+form.url.oninput = validation
+
+
+console.log(form.url);
+
+
+
+
 
 form.onsubmit = (event) => {
     event.preventDefault()
-    const { url: { value: url } } = event.target
-
-    const urlRegEx = /https?:\/\/[\w]*\.[\w]*[\.|\/]?[\w]*[\.|\/]?[\w|\/-]*/
-
-    console.log(urlRegEx.test(url));
+    const {
+        url: { value: url },
+        email: { value: email }
+    } = event.target
 
     if (urlRegEx.test(url)) {
         urlValidation.innerText = 'Si bro ya pasa👍'
-        setTimeout(() => {
-            urlValidation.innerText = ''
-        },2000)
+        urlValidation.style.color = 'green'
     } else {
-        urlValidation.innerText = 'No bro, no pasa😡'
-        setTimeout(() => {
-            urlValidation.innerText = ''
-        },2000)
+        urlValidation.innerText = 'No es válido😡'
+    }
+
+    if (emailRegEx.test(email)) {
+        emailValidation.innerText = 'Es válido'
+    } else {
+        emailValidation.innerText = 'No es valido'
     }
 } 
