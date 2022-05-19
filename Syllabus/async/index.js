7e6db54d5b9f14dfce1d2421ac78f4bfa2a74c1a
EdgarMTdev <edgarmontiel961@gmail.com>
2022-05-18 19:48:06 -0500

    Add fetch

--- Syllabus/async/index.js
+++ Syllabus/async/index.js
@@ -4,7 +4,22 @@ const root = document.getElementById('root')
 
 let characters = []
 
-fetch('https://rickandmortyapi.com/api/character')
+function renderCharacters(data) {
+    data.forEach(item => {
+        const div = document.createElement('article')
+        const p = document.createElement('p')
+        const img = document.createElement('img')
+        p.innerText = item.name
+        img.src = item.image
+        div.appendChild(p)
+        div.appendChild(img)
+        root.appendChild(div)
+    });
+}
+
+fetch('https://rickandmortyapi.com/api/character') //Fetch no lanza excepciones para getionarlas con catch()
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        renderCharacters(data.results)
+    })
     .catch(error => console.log(error))
