d6fae99bd05743bd64e8572893fbe821b8ca4aba
EdgarMTdev <edgarmontiel961@gmail.com>
2022-06-16 20:13:57 -0500

    add: create figures with p5.js

--- Syllabus/p5/libs/sketch.js
+++ Syllabus/p5/libs/sketch.js
@@ -1,15 +1,32 @@
+'use strict'
+
+const canvasWidth = window.innerWidth
+const canvasHeight = window.innerHeight
+
 function setup() {
-    createCanvas(500, 120)
+    createCanvas(canvasWidth, canvasHeight)
+    background(234, 194, 227)
+    rectMode(CENTER)
 }
 
 function draw(){
+    const x = canvasWidth/2
+    const y = canvasHeight/2
+
+    const rectangle = (w, h) => rect(x, y, w, h)
     // background(200)
     // ellipse(50, 50, 80, 80) // -> (left, top, width, height)
 
-    if (mouseIsPressed) {
-        fill(0)
-    } else {
-        fill(255)
-    }
-    ellipse(mouseX, mouseY, 10, 10)
+    // if (mouseIsPressed) {
+    //     ellipse(mouseX, mouseY, 10, 10)
+    //     fill(0)
+    // } else {
+    //     fill(255)
+    // }
+    fill(0)
+    rectangle(100, 100)
+    fill(256)
+    rectangle(80, 80)
+    fill(0)
+    rectangle(60, 60)
 }
