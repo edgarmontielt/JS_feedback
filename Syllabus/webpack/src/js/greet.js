faf5dcbbbbd084b83a6c38663681baf57ff494af
EdgarMTdev <edgarmontiel961@gmail.com>
2022-07-05 20:16:12 -0500

    add: webpack config

---
+++ Syllabus/webpack/src/js/greet.js
@@ -0,0 +1,3 @@
+export function greet() {
+    alert('Hi, this is Webpack')
+}
---
+++ Syllabus/webpack/src/js/index.js
@@ -0,0 +1,6 @@
+'use strict'
+import { greet } from '../js/greet'
+
+const name = 'Edgar'
+console.log(`Hello ${name}`);
+greet()
